bdf2ea0163c4199e0bf85ff8ca80665b2a0caf84
anotherangrymouse <grillopress.com@gmail.com>
2014-10-02 16:03:04 +0000

    add breadcrumb test

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-service-home.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-service-home.php
@@ -18,10 +18,25 @@ Template Name: Service home page
     </div>
   </div>
 
+
+
   <div class="container">
 			<div id="content" class="clearfix row">
 			
 				<div id="main" class="col col-sm-8 clearfix" role="main">
+          
+          <ol class="breadcrumb">
+            <li><a href="#">Home</a>
+            </li>
+            <li class="active">Services</li>
+          </ol>
+
+          <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie
+            consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et
+            accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+            delenit augue duis dolore te feugait nulla facilisi.</p>
+          
+          
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 					
 						<section class="post_content">
