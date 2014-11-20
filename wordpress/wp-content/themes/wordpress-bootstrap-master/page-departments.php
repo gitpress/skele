8aba9e2cfbdf48a8d3591583d19ab4e920ad56c5
anotherangrymouse <grillopress.com@gmail.com>
2014-11-20 12:22:09 +0000

    add team section to department template

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-departments.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-departments.php
@@ -11,7 +11,7 @@ Template Name: Department Homepage
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-sm-8">
-          <h1><?php the_title(); ?></h1>
+          <h1>Medical Imaging and Medical Physics Directorate</h1>
           <?php echo '<p class="lead">' . get_post_meta($post->ID, 'page_desc', true) . '</p>'; ?>
         </div>
         <div class="col-md-4 col-sm-4" id="mimp-service-buttons">
@@ -25,65 +25,136 @@ Template Name: Department Homepage
     </div>
   </div>
 
+
 <div id="">
- <div class="container">
+  <div class="container">
     <div class="row">
-      <div class="container">
-            <div class="col-md-12">
+      <div class="col-sm-8">
 
+          <h1>What we do</h1>
+          <p>The Medical Imaging and Medical Physics Directorate brings together two of Sheffield Teaching Hospitals departments who provide closely related services.</p>
+        <p>Accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+          
+          
+          <div id="mimp-nav-blocks">
 
-              
-            <div class="row" id="mimp-nav-blocks">
-              <div class="col-md-3">
-                
-                <a href="#"><img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
-                <div class="mimp-branded-block" id="find-us">
-                  <h4>Medical Imaging &amp; Clinical Engineering</h4>
-                  <footer><em>Find out more about our three world-class departments</em>
-                  </footer>
-                </div>
+            <div class="row">
+              <div class="col-md-6">
+
+                <a href="#"><img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg"
+                  class="img-responsive">
+                  <div class="mimp-branded-block" id="find-us">
+                    <h4>Medical Imaging &amp; Clinical Engineering</h4>
+                    <footer><em>Find out more about our three world-class departments</em>
+                    </footer>
+                  </div>
                 </a>
-                 
+
               </div>
-              <div class="col-md-3">
-                
-                <a href="#"><img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
-                                <div class="mimp-branded-block" id="find-us">
-                  <h4>Radiology Department</h4>
-                  <footer><em>Find out more about our three world-class departments</em>
-                  </footer>
-                </div>
+              <div class="col-md-6">
+
+                <a href="#"><img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg"
+                  class="img-responsive">
+                  <div class="mimp-branded-block" id="find-us">
+                    <h4>Radiology Department</h4>
+                    <footer><em>Find out more about our three world-class departments</em>
+                    </footer>
+                  </div>
                 </a>
-                  
+
               </div>
-              <div class="col-md-3">
-               
-                <a href="#"><img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
-                                <div class="mimp-branded-block" id="find-us">
-                  <h4>Our Professional Services</h4>
-                  <footer><em>Find out more about our three world-class departments</em>
-                  </footer>
-                </div>
-                </a>
-                  
-              </div>
-              <div class="col-md-3">
-               
-                <a href="#"><img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
-                                <div class="mimp-branded-block" id="find-us">
-                 <h4>Meet the team</h4>
-                  <footer><em>Find out more about our three world-class departments</em>
-                  </footer>
-                </div>
-                </a>
-                  
+
+            </div>
+          </div>
+          <p>Our Radiology department employs highly skilled teams of radiologists, radiographers and nurses who utilise their expertise in the acquisition and  interpretation of images of the body for the diagnosis of disease. They apply the latest technology using X-Rays, Magnetic Resonance (MRI), Ultrasound, and Computed Tomography to produce images of the finest quality, whilst maintaining patient care at the highest levels. Their imaging services extend to cover the broadest range of imaging in both routine and acute settings and they play a vital role in the clinical diagnosis of injury and disease.  </p>      
+          <p>Medical Physics and Clinical Engineering teams work alongside them, to ensure that patients are adequately protected from the low levels of radiation used in imaging and to optimise the equipment such that acquired images are of the highest quality.  They further apply their knowledge and skills of physics and engineering to provide innovative services in  specialist areas of heathcare: Audiological Science, Clinical Engineering, Visual Electrophysiology, Scientific Computing and Informatics,  Nuclear Medicine, and  Gamma Knife Radiosurgery. </p>
+        
+      </div>
+      
+      <div class="col-sm-4">
+        <h1>Our Services</h1>
+        <img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
+        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        <a href="#" class="btn btn-default btn-block" role="button">Audiological Science</a>
+        <img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
+        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        <a href="#" class="btn btn-default btn-block" role="button">Audiological Science</a>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="mimp-team-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Our team</h1>
+        
+        <div class="row">
+          <div class="col-md-6">
+            <div class="media">
+              <a class="pull-left" href="#">
+                <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png"
+                alt="Media Object" height="100px">
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                text.
               </div>
             </div>
-              
+            
+            <div class="media">
+              <a class="pull-left" href="#">
+                <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png"
+                alt="Media Object" height="100px">
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                text.
+              </div>
+            </div>
+          </div>
+          
+           <div class="col-md-6">
+              <div class="media">
+                <a class="pull-left" href="#">
+                  <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png"
+                  alt="Media Object" height="100px">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                  text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                  text.
+                </div>
+              </div>
+              <div class="media">
+                <a class="pull-left" href="#">
+                  <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png"
+                  alt="Media Object" height="100px">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                  text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                  text.
+                </div>
+               </div>
           </div>
         </div>
-   </div>
- </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      </div>
+    </div>
+  </div>
 </div>
 
 
