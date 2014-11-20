7d2576ce07f5498ce8625c3327d66fbcc4f6b994
anotherangrymouse <grillopress.com@gmail.com>
2014-11-20 14:22:52 +0000

    add additional styles to department page mockup

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-departments.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-departments.php
@@ -26,15 +26,14 @@ Template Name: Department Homepage
   </div>
 
 
-<div id="">
+<div id="mimp-department-intro">
   <div class="container">
     <div class="row">
       <div class="col-sm-8">
 
-          <h1>What we do</h1>
+          <h1 class="under-margin">What we do</h1>
           <p>The Medical Imaging and Medical Physics Directorate brings together two of Sheffield Teaching Hospitals departments who provide closely related services.</p>
-        <p>Accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-          
+           
           
           <div id="mimp-nav-blocks">
 
@@ -57,6 +56,7 @@ Template Name: Department Homepage
                   class="img-responsive">
                   <div class="mimp-branded-block" id="find-us">
                     <h4>Radiology Department</h4>
+                    <br>
                     <footer><em>Find out more about our three world-class departments</em>
                     </footer>
                   </div>
@@ -68,17 +68,17 @@ Template Name: Department Homepage
           </div>
           <p>Our Radiology department employs highly skilled teams of radiologists, radiographers and nurses who utilise their expertise in the acquisition and  interpretation of images of the body for the diagnosis of disease. They apply the latest technology using X-Rays, Magnetic Resonance (MRI), Ultrasound, and Computed Tomography to produce images of the finest quality, whilst maintaining patient care at the highest levels. Their imaging services extend to cover the broadest range of imaging in both routine and acute settings and they play a vital role in the clinical diagnosis of injury and disease.  </p>      
           <p>Medical Physics and Clinical Engineering teams work alongside them, to ensure that patients are adequately protected from the low levels of radiation used in imaging and to optimise the equipment such that acquired images are of the highest quality.  They further apply their knowledge and skills of physics and engineering to provide innovative services in  specialist areas of heathcare: Audiological Science, Clinical Engineering, Visual Electrophysiology, Scientific Computing and Informatics,  Nuclear Medicine, and  Gamma Knife Radiosurgery. </p>
-        
+           <a href="#" class="btn btn-mimp">Find out more about our commercial services</a> <a href="#" class="btn btn-link">Patient information</a>
       </div>
       
       <div class="col-sm-4">
-        <h1>Our Services</h1>
+        <h1 class="under-margin">Our Services</h1>
         <img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
         <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-        <a href="#" class="btn btn-default btn-block" role="button">Audiological Science</a>
+        <a href="#" class="btn btn-mimp btn-block" role="button">Audiological Science</a>
         <img src="http://news.bbcimg.co.uk/media/images/78466000/jpg/_78466685_healthcare_team-spl.jpg" class="img-responsive">
         <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-        <a href="#" class="btn btn-default btn-block" role="button">Audiological Science</a>
+        <a href="#" class="btn btn-mimp btn-block" role="button">Audiological Science</a>
         
       </div>
     </div>
@@ -89,7 +89,7 @@ Template Name: Department Homepage
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h1>Our team</h1>
+        <h1 class="under-margin">Our management team</h1>
         
         <div class="row">
           <div class="col-md-6">
@@ -144,17 +144,45 @@ Template Name: Department Homepage
           </div>
         </div>
         
-        
-        
-        
-        
-        
-        
-        
-        
+         <div class="row">
+           <div class="col-md-12">
+             <br>
+             <a href="#" class="btn btn-link btn-white">Find out more about our staff</a>
+           </div>
+           
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<div id="mimp-contact-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+       
+          <div class="row">
+            <div class="col-md-6" id="">
+              <h2 class="no-margin-header">Royal Hallamshire Hospital</h2>
+              <p>paragraph about something to do with turning up at the Hallamshire. This is some sample text. This is some sample text. This is some sample
+                  text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                  text.</p>
+              <address>
+                  <a href="http://www.sth.nhs.uk/our-hospitals/royal-hallamshire-hospital/contact-us">Royal Hallamshire Hospital</a><br>
+                  Glossop Road<br>
+                  Sheffield<br>
+                  S10 2JF<br>
+              </address>
+            </div>
+            <div class="col-md-6" id="">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2380.0252728582786!2d-1.492584114025166!3d53.3785972830175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48798276f8fdde19%3A0x431cbeb702d1a974!2sRoyal+Hallamshire+Hospital!5e0!3m2!1sen!2suk!4v1416486453029" width="auto" height="400" frameborder="0" style="border:0;min-height:100%;min-width:100%;"></iframe>
+            </div>
+          </div>        
       </div>
     </div>
   </div>
+  
 </div>
 
 
