b9879cf51ac713dfa1b41288f3bab8fb0b98dda0
anotherangrymouse <grillopress.com@gmail.com>
2014-11-24 16:49:51 +0000

    update frontpage template with blue striped section and remove shadow css

--- wordpress/wp-content/themes/wordpress-bootstrap-master/page-homepage.php
+++ wordpress/wp-content/themes/wordpress-bootstrap-master/page-homepage.php
@@ -156,4 +156,109 @@ Template Name: Homepage
     </div>
   </div>
 
+<div id="mimp-team-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="under-margin">Major Section</h1>
+        
+        <div class="row">
+          <div class="col-md-6">
+            <div class="media">
+              <a class="pull-left" href="#">
+                <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png" alt="Media Object" height="100px">
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                text.
+              </div>
+            </div>
+            
+            <div class="media">
+              <a class="pull-left" href="#">
+                <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png" alt="Media Object" height="100px">
+              </a>
+              <div class="media-body">
+                <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                text.
+              </div>
+            </div>
+          </div>
+          
+           <div class="col-md-6">
+              <div class="media">
+                <a class="pull-left" href="#">
+                  <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png" alt="Media Object" height="100px">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                  text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                  text.
+                </div>
+              </div>
+              <div class="media">
+                <a class="pull-left" href="#">
+                  <img class="media-object" src="http://upload.wikimedia.org/wikipedia/en/b/b1/Portrait_placeholder.png" alt="Media Object" height="100px">
+                </a>
+                <div class="media-body">
+                  <h4 class="media-heading">Media heading</h4> This is some sample text. This is some sample text. This is some sample
+                  text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                  text.
+                </div>
+               </div>
+          </div>
+        </div>
+        
+         <div class="row">
+           <div class="col-md-12">
+             <br>
+             <a href="#" class="btn btn-link btn-white">Find out more about our staff</a>
+           </div>
+           
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<div id="mimp-contact-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+       
+          <div class="row">
+            <div class="col-md-6" id="">
+              <h2 class="no-margin-header">Royal Hallamshire Hospital</h2>
+              <p>paragraph about something to do with turning up at the Hallamshire. This is some sample text. This is some sample text. This is some sample
+                  text. This is some sample text. This is some sample text. This is some sample text. This is some sample
+                  text.</p>
+              <address>
+                  <a href="http://www.sth.nhs.uk/our-hospitals/royal-hallamshire-hospital/contact-us">Royal Hallamshire Hospital</a><br>
+                  Glossop Road<br>
+                  Sheffield<br>
+                  S10 2JF<br>
+              </address>
+              
+              <h3>Patient Information</h3>
+              <p>If you have an appointment with us, or are looking for information about what to expect, please see our dedicated pages:</p>
+<a href="#">How do I get a referral?</a><br>
+<a href="#">How do I get there</a><br>
+<a href="#">What happens at an appointment?</a><br>
+<a href="#">What happens after an appointment?</a><br>
+
+
+            </div>
+            <div class="col-md-6" id="">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2380.0252728582786!2d-1.492584114025166!3d53.3785972830175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48798276f8fdde19%3A0x431cbeb702d1a974!2sRoyal+Hallamshire+Hospital!5e0!3m2!1sen!2suk!4v1416486453029" width="auto" height="400" frameborder="0" style="border:0;min-height:100%;min-width:100%;"></iframe>
+            </div>
+          </div>        
+      </div>
+    </div>
+  </div>
+  
+</div>
+
 <?php get_footer(); ?>
